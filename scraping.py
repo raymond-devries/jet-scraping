1b449bf428468becd1b5926d850835825b44844d
Raymond DeVries <raymond.l.devries@gmail.com>
2020-10-06 21:42:44 -0700

    Added scraper to gather high level data on all jets.

---
+++ scraping.py
@@ -0,0 +1,29 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
+
+class JetsSpider(scrapy.Spider):
+    name = "jet-spider"
+    start_urls = ["https://en.wikipedia.org/wiki/List_of_fighter_aircraft"]
+
+    def parse(self, response, **kwargs):
+        table = response.xpath('//*[@id="mw-content-text"]/div[1]/table')
+        rows = table.xpath("//tr")
+        for row in rows[1:]:
+            table_values = row.css("td::text").getall()
+            yield {
+                "link": row.css("a::attr(href)").get(),
+                "name": row.css("a::text").get(),
+                "country": table_values[0],
+                "year": table_values[1]
+            }
+
+
+if __name__ == '__main__':
+    process = CrawlerProcess(settings={
+        "FEEDS": {
+            "planes.csv": {"format": "csv"}
+        },
+    })
+    process.crawl(JetsSpider)
+    process.start()
